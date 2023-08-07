d530b83f9535b3971929cd24099c4f7a0e968f96
Dominik Pieper <dominik@pieper.io>
2023-08-07 09:11:39 +0200

    Add personal information and remove Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [tailwind(), mdx(), sitemap()],
   site: "https://pieper.io",
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkToc]
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,8 +16,8 @@ module.exports = {
 			},
 			colors: {
 				'pacamara-primary': '#003049',
-				'pacamara-secondary': '#B2A4FF',
-				'pacamara-accent': '#FFB4B4',
+				'pacamara-secondary': '#816fe2',
+				'pacamara-accent': '#f4c161',
 				'pacamara-dark': '#000E14',
 				'pacamara-white': '#ffffff',
 			},
